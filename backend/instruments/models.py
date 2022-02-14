ef77795c8cd371d9713475d1f6fc15ad2507b9a7
Tuomas Siipola <tuomas.siipola@fmi.fi>
2022-02-14 12:29:59 +0200

    Don't require instrument image

--- backend/instruments/models.py
+++ backend/instruments/models.py
@@ -44,7 +44,7 @@ class Instrument(models.Model):
     )
     commission_date = models.DateField(null=True, blank=True)
     decommission_date = models.DateField(null=True, blank=True)
-    image = models.ImageField(null=True)
+    image = models.ImageField(null=True, blank=True)
     types = models.ManyToManyField(
         Type, help_text="Classification of the type of the instrument.", blank=True
     )
