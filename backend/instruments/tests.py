a0bff95d10b05fffa721db64a39a8fb04d474fe9
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-03-10 13:47:45 +0200

    Fix instrument type on index page

--- backend/instruments/tests.py
+++ backend/instruments/tests.py
@@ -349,6 +349,16 @@ class ComponentsTest(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def test_index_html(self):
+        response = self.client.get("/")
+        response_decoded = response.content.decode("utf-8")
+        self.assertEqual(response.status_code, 200)
+        test_strings = (
+            '<a href="http://localhost:8000/instrument/90845957-31eb-4900-89a5-78696ec0453d">My weather station</a><br>Weather station type',
+        )
+        for test_string in test_strings:
+            self.assertInHTML(test_string, response_decoded)
+
     def test_parent_xml(self):
         response = self.client.get(
             "/instrument/90845957-31eb-4900-89a5-78696ec0453d.xml"
